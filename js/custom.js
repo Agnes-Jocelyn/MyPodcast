c023d2677e3933243d158a3941b892d3382381d2
Agnes Jocelyn <Agnesjocelyn95@gmail.com>
2020-05-01 18:34:22 +0700

    Initial Project

---
+++ js/custom.js
@@ -0,0 +1,350 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Favs
+5. Init SVG
+6. Init Gallery
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	setHeader();
+	initMenu();
+	initFavs();
+	initSvg();
+	initGallery();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+
+		setTimeout(function()
+		{
+			$(window).trigger('resize.px.parallax');
+		}, 375);
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		var header = $('.header');
+
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if($('.menu').length)
+		{
+			var menu = $('.menu');
+			var hamburger = $('.hamburger');
+			var close = $('.menu_close');
+
+			hamburger.on('click', function()
+			{
+				menu.toggleClass('active');
+			});
+
+			close.on('click', function()
+			{
+				menu.toggleClass('active');
+			});
+		}
+	}
+
+	/* 
+
+	4. Init Favs
+
+	*/
+
+	function initFavs()
+	{
+		if($('.show_fav_icon').length)
+		{
+			var icons = $('.show_fav_icon');
+			icons.each(function()
+			{
+				var icon = $(this);
+				icon.on('click', function()
+				{
+					if(icon.hasClass('active'))
+					{
+						icon.removeClass('active');
+					}
+					else
+					{
+						icon.addClass('active');
+					}
+				});
+			});
+		}
+	}
+
+	/* 
+
+	5. Init SVG
+
+	*/
+
+	function initSvg()
+	{
+		if($('img.svg').length)
+		{
+			jQuery('img.svg').each(function()
+			{
+				var $img = jQuery(this);
+				var imgID = $img.attr('id');
+				var imgClass = $img.attr('class');
+				var imgURL = $img.attr('src');
+
+				jQuery.get(imgURL, function(data)
+				{
+					// Get the SVG tag, ignore the rest
+					var $svg = jQuery(data).find('svg');
+
+					// Add replaced image's ID to the new SVG
+					if(typeof imgID !== 'undefined') {
+					$svg = $svg.attr('id', imgID);
+					}
+					// Add replaced image's classes to the new SVG
+					if(typeof imgClass !== 'undefined') {
+					$svg = $svg.attr('class', imgClass+' replaced-svg');
+					}
+
+					// Remove any invalid XML tags as per http://validator.w3.org
+					$svg = $svg.removeAttr('xmlns:a');
+
+					// Replace image with new SVG
+					$img.replaceWith($svg);
+				}, 'xml');
+			});
+		}	
+	}
+
+	/* 
+
+	6. Init Gallery
+
+	*/
+
+	function initGallery()
+	{
+		if($('.gallery_item').length)
+		{
+			$('.colorbox').colorbox(
+			{
+				rel:'colorbox',
+				photo: true,
+				maxWidth:'95%',
+				maxHeight:'95%'
+			});
+		}
+	}
+	$('.btn-counter').on('click', function(event, count) {
+	  event.preventDefault();
+	  
+	  var $this = $(this),
+	      count = $this.attr('data-count'),
+	      active = $this.hasClass('active'),
+	      multiple = $this.hasClass('multiple-count');
+	  
+	  // First method, allows to add custom function
+	  // Use when you want to do an ajax request
+	  /* if (multiple) {
+	  $this.attr('data-count', ++count);
+	  // Your code here
+	  } else {
+	  $this.attr('data-count', active ? --count : ++count).toggleClass('active');
+	  // Your code here
+	  } */
+	  
+	  // Second method, use when ... I dunno when but it looks cool and that's why it is here
+	  $.fn.noop = $.noop;
+	  $this.attr('data-count', ! active || multiple ? ++count : --count  )[multiple ? 'noop' : 'toggleClass']('active');
+	  
+	});
+(function($) {
+  "use strict";
+  $.fn.sliderResponsive = function(settings) {
+    
+    var set = $.extend( 
+      {
+        slidePause: 5000,
+        fadeSpeed: 800,
+        autoPlay: "on",
+        showArrows: "off", 
+        hideDots: "off", 
+        hoverZoom: "on",
+        titleBarTop: "off"
+      },
+      settings
+    ); 
+    
+    var $slider = $(this);
+    var size = $slider.find("> div").length; //number of slides
+    var position = 0; // current position of carousal
+    var sliderIntervalID; // used to clear autoplay
+      
+    // Add a Dot for each slide
+    $slider.append("<ul></ul>");
+    $slider.find("> div").each(function(){
+      $slider.find("> ul").append('<li></li>');
+    });
+      
+    // Put .show on the first Slide
+    $slider.find("div:first-of-type").addClass("show");
+      
+    // Put .showLi on the first dot
+    $slider.find("li:first-of-type").addClass("showli")
+
+     //fadeout all items except .show
+    $slider.find("> div").not(".show").fadeOut();
+    
+    // If Autoplay is set to 'on' than start it
+    if (set.autoPlay === "on") {
+        startSlider(); 
+    } 
+    
+    // If showarrows is set to 'on' then don't hide them
+    if (set.showArrows === "on") {
+        $slider.addClass('showArrows'); 
+    }
+    
+    // If hideDots is set to 'on' then hide them
+    if (set.hideDots === "on") {
+        $slider.addClass('hideDots'); 
+    }
+    
+    // If hoverZoom is set to 'off' then stop it
+    if (set.hoverZoom === "off") {
+        $slider.addClass('hoverZoomOff'); 
+    }
+    
+    // If titleBarTop is set to 'on' then move it up
+    if (set.titleBarTop === "on") {
+        $slider.addClass('titleBarTop'); 
+    }
+
+    // function to start auto play
+    function startSlider() {
+      sliderIntervalID = setInterval(function() {
+        nextSlide();
+      }, set.slidePause);
+    }
+    
+    // on mouseover stop the autoplay
+    $slider.mouseover(function() {
+      if (set.autoPlay === "on") {
+        clearInterval(sliderIntervalID);
+      }
+    });
+      
+    // on mouseout starts the autoplay
+    $slider.mouseout(function() {
+      if (set.autoPlay === "on") {
+        startSlider();
+      }
+    });
+
+    //on right arrow click
+    $slider.find("> .right").click(nextSlide)
+
+    //on left arrow click
+    $slider.find("> .left").click(prevSlide);
+      
+    // Go to next slide
+    function nextSlide() {
+      position = $slider.find(".show").index() + 1;
+      if (position > size - 1) position = 0;
+      changeCarousel(position);
+    }
+    
+    // Go to previous slide
+    function prevSlide() {
+      position = $slider.find(".show").index() - 1;
+      if (position < 0) position = size - 1;
+      changeCarousel(position);
+    }
+
+    //when user clicks slider button
+    $slider.find(" > ul > li").click(function() {
+      position = $(this).index();
+      changeCarousel($(this).index());
+    });
+
+    //this changes the image and button selection
+    function changeCarousel() {
+      $slider.find(".show").removeClass("show").fadeOut();
+      $slider
+        .find("> div")
+        .eq(position)
+        .fadeIn(set.fadeSpeed)
+        .addClass("show");
+      // The Dots
+      $slider.find("> ul").find(".showli").removeClass("showli");
+      $slider.find("> ul > li").eq(position).addClass("showli");
+    }
+
+    return $slider;
+  };
+})(jQuery);
+
+
+ 
+//////////////////////////////////////////////
+// Activate each slider - change options
+//////////////////////////////////////////////
+$(document).ready(function() {
+  
+  $("#slider1").sliderResponsive({
+  // Using default everything
+    // slidePause: 5000,
+    // fadeSpeed: 800,
+    // autoPlay: "on",
+    // showArrows: "off", 
+    // hideDots: "off", 
+    // hoverZoom: "on", 
+    // titleBarTop: "off"
+  });  
+});
+});
